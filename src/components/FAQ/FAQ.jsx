512405a283afe60d95ee9ed7d3bd812bc291a4a1
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-08-13 09:50:57 -0400

    Remove warning about key indexes

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 import "./FAQ.css";
 import FAQTerminal from "../FAQTerminal/FAQTerminal";
+import { faq } from "./questions.js";
 
 export default function FAQ() {
-    const faq = {
-        "What is a hackathon?": " A hackathon is an event where participants work together in teams to brainstorm, develop, and prototype software solutions or projects that aim to address a specific problem or challenge.",
-        "Who can participate?": "Any high school student in Canada can participate, exceptions can be made for elementary students though!",
-        "What do I need to bring?": "You should bring a laptop, charger, don't worry about teams and projects, we'll help you with that!",
-        "How much does it cost?": "It's completely free! We'll even provide cool swag, food, and prizes for non hackathon related competitions!",
-        "How will teams work?": "Teams can be up to 4 people, or solo if you want to work alone! We'll help you find a team if you don't have one.",
-        "Do I need to know how to code?": "Coding experience is not a requirement to participate in our hackathon. We welcome people with a range of skills such as marketing, design, and more. Participants are also encouraged to form teams to complement each other's strengths and skill sets.",
-    }
+    const faqStuff = faq.map(data => 
+        <div className="faq-item" key={data.id}>
+            <FAQTerminal question={data.question} answer={data.answer} />
+        </div>
+    );
 
     return (
         <section id="faq">
             <h1>Frequently Asked Questions</h1>
             <div className="faq">
-                {Object.keys(faq).map((key) => {
-                    return (
-                        <div className="faq-item">
-                            <FAQTerminal key={key} question={key} answer={faq[key]} />
-                        </div>
-                    );
-                })
-                }
+                {faqStuff}
             </div>
         </section>
     );
---
+++ src/components/FAQ/questions.js
@@ -0,0 +1,32 @@
+export const faq = [
+    {
+        id: 0,
+        question: "What is a hackathon?",
+        answer: "A hackathon is an event where participants work together in teams to brainstorm, develop, and prototype software solutions or projects that aim to address a specific problem or challenge."
+    },
+    {
+        id: 1,
+        question: "Who can participate?",
+        answer: "Any high school student in Canada can participate, exceptions can be made for elementary students though!"
+    },
+    {
+        id: 2,
+        question: "What do I need to bring?",
+        answer: "You should bring a laptop, charger, don't worry about teams and projects, we'll help you with that!"
+    },
+    {
+        id: 3,
+        question: "How much does it cost?",
+        answer: "It's completely free! We'll even provide cool swag, food, and prizes for non hackathon related competitions!"
+    },
+    {
+        id: 4,
+        question: "How will teams work?",
+        answer: "Teams can be up to 4 people, or solo if you want to work alone! We'll help you find a team if you don't have one."
+    },
+    {
+        id: 5,
+        question: "Do I need to know how to code?",
+        answer: "Coding experience is not a requirement to participate in our hackathon. We welcome people with a range of skills such as marketing, design, and more. Participants are also encouraged to form teams to complement each other's strengths and skill sets."
+    }
+];
