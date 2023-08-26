521b0f7ae80334c89ba79db6afa5f1433df7573f
Ben <ben.zhou.2006@gmail.com>
2023-08-26 02:06:28 -0400

    Add ref noreferrer

--- src/components/SponsorPanel/SponsorPanel.jsx
+++ src/components/SponsorPanel/SponsorPanel.jsx
@@ -6,7 +6,7 @@ export default function SponsorPanel () {
     return (
         <section id="SponsorPanel">
             <div className="SponsorPanel">
-                <a href="https://aph.hdsb.ca/" target="_blank" class="sponsorbox">
+                <a href="https://aph.hdsb.ca/" target="_blank" class="sponsorbox" rel="noreferrer">
                     <img src={APHS} alt="Abbey Park Icon" />
                 </a>
             </div>
