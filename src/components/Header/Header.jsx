a943bc96545840fc0694e1d47eabb0819cb185aa
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-08-30 23:12:01 -0400

    Finish basic layout for landing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,19 +9,25 @@ import Landing2 from "../../assets/landing-2.jpg";
 export default function Header() {
     return (
         <section id="home">
-            <div className="banner">
-                <div className="banner-content">
-                    <div className="landing-images">
-                        <img className="landing-1" src={Landing1} alt="Landing 1" />
-                        <img className="landing-2" src={Landing2} alt="Landing 2" />
-                    </div>
-                    <div className="banner-text">
-                        <h1>EUREKA!</h1>
-                        <h2>2024</h2>
-                        <Button text="Register" link="https://google.com" />
-                    </div>
-                    <div className="banner-logo-container">
-                        <img className="banner-logo" src={Logo} alt="Eureka Logo" />
+            <div className="header">
+                <div className="banner">
+                    <div className="banner-content">
+                        <div className="landing-images">
+                            <div className="landing-1 banner-image-glow">
+                                <img src={Landing1} alt="Landing 1" />
+                            </div>
+                            <div className="landing-2 banner-image-glow">
+                                <img src={Landing2} alt="Landing 2" />
+                            </div>
+                        </div>
+                        <div className="banner-text">
+                            <h1>EUREKA!</h1>
+                            <h2>2024</h2>
+                            <Button text="Register" link="https://google.com" />
+                        </div>
+                        <div className="banner-logo-container">
+                            <img className="banner-logo" src={Logo} alt="Eureka Logo" />
+                        </div>
                     </div>
                 </div>
             </div>
