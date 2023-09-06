ee3751eeef6b18ac4f9617ad9cb9e01a66ed9ac0
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-06 19:16:31 -0400

    Finish adjusting images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,23 +13,33 @@ export default function Header() {
                 <div className="banner-content">
                     <div className="landing-images">
                         <div className="landing-1 banner-image-glow">
-                            <img src={Landing1} alt="Landing 1" />
+                            <img
+                                src={Landing1}
+                                className="landing-image"
+                                alt="Landing 1"
+                            />
                         </div>
                         <div className="landing-2 banner-image-glow">
-                            <img src={Landing2} alt="Landing 2" />
+                            <img
+                                src={Landing2}
+                                className="landing-image"
+                                alt="Landing 2"
+                            />
                         </div>
                     </div>
-                    <div className="banner-text">
-                        <h1>EUREKA!</h1>
-                        <h3>2024</h3>
-                        <Button text="Register" link="https://google.com" />
-                    </div>
-                    <div className="banner-logo-container">
-                        <img
-                            className="banner-logo"
-                            src={Logo}
-                            alt="Eureka Logo"
-                        />
+                    <div className="banner-text-logo">
+                        <div className="banner-text">
+                            <h1>EUREKA!</h1>
+                            <h3>2024</h3>
+                            <Button text="Register" link="https://google.com" />
+                        </div>
+                        <div className="banner-logo-container">
+                            <img
+                                className="banner-logo"
+                                src={Logo}
+                                alt="Eureka Logo"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
