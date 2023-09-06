d626a4c1091cc76e980f2d44492663bfd7b8834e
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-06 17:37:37 -0400

    Finish adjusting styles for h1 h2 and h3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,30 +9,30 @@ import Landing2 from "../../assets/landing-2.webp";
 export default function Header() {
     return (
         <section id="home">
-                <div className="banner">
-                    <div className="banner-content">
-                        <div className="landing-images">
-                            <div className="landing-1 banner-image-glow">
-                                <img src={Landing1} alt="Landing 1" />
-                            </div>
-                            <div className="landing-2 banner-image-glow">
-                                <img src={Landing2} alt="Landing 2" />
-                            </div>
+            <div className="banner">
+                <div className="banner-content">
+                    <div className="landing-images">
+                        <div className="landing-1 banner-image-glow">
+                            <img src={Landing1} alt="Landing 1" />
                         </div>
-                        <div className="banner-text">
-                            <h1>EUREKA!</h1>
-                            <h2>2024</h2>
-                            <Button text="Register" link="https://google.com" />
-                        </div>
-                        <div className="banner-logo-container">
-                            <img
-                                className="banner-logo"
-                                src={Logo}
-                                alt="Eureka Logo"
-                            />
+                        <div className="landing-2 banner-image-glow">
+                            <img src={Landing2} alt="Landing 2" />
                         </div>
                     </div>
+                    <div className="banner-text">
+                        <h1>EUREKA!</h1>
+                        <h3>2024</h3>
+                        <Button text="Register" link="https://google.com" />
+                    </div>
+                    <div className="banner-logo-container">
+                        <img
+                            className="banner-logo"
+                            src={Logo}
+                            alt="Eureka Logo"
+                        />
+                    </div>
                 </div>
+            </div>
         </section>
     );
 }
