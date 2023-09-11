ba7be28adec78dfa27fcd9fe71b276f3e353b346
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-10 21:39:08 -0400

    Use thumbnail instead of massive image

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -9,11 +9,23 @@ export default function Description() {
             <div className="description">
                 <div className="description-left">
                     Abbey Park's Very Own Hackathon!
-                    <h6 className="description-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor eu nisi sed consectetur. Praesent ullamcorper ante sem. Cras sed imperdiet arcu. Cras faucibus, mi sed mattis tristique, metus libero interdum lectus, vel vestibulum enim ipsum in libero. Pellentesque dictum vulputate semper. Nullam varius tempus nulla, ac lacinia mauris mattis vel. Nulla eu quam ut ante ultricies tincidunt eget in urna.</h6>
+                    <h6 className="description-left">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse auctor eu nisi sed consectetur. Praesent
+                        ullamcorper ante sem. Cras sed imperdiet arcu. Cras
+                        faucibus, mi sed mattis tristique, metus libero interdum
+                        lectus, vel vestibulum enim ipsum in libero.
+                        Pellentesque dictum vulputate semper. Nullam varius
+                        tempus nulla, ac lacinia mauris mattis vel. Nulla eu
+                        quam ut ante ultricies tincidunt eget in urna.
+                    </h6>
                 </div>
-                <img className="description-right glow" src="https://via.placeholder.com/300" alt="placeholder" />
+                <img
+                    className="description-right glow"
+                    src="https://via.placeholder.com/300"
+                    alt="placeholder"
+                />
             </div>
-
         </div>
     );
 }
