6ba0b227dc22d966c3d1f50f80d58a95406e4420
Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-13 12:40:11 -0400

    Update styling to start mobile

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import { useScrollDirection } from "../../hooks/useScrollDirection";
 
@@ -7,6 +8,11 @@ import Logo from "../../assets/EurekaIcon2024.png";
 
 export default function NavBar() {
     const scrollingUp = useScrollDirection();
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen);
+    }
 
     // Some useless code here but if we ever wanna change it it's here.
     // Decided to remove scroll effects for above
@@ -33,6 +39,20 @@ export default function NavBar() {
                 <a href="/#faq">FAQ</a>
                 <a href="/#sponsors">Sponsors</a>
             </ul>
+
+            <div className="hamburger-menu" onClick={toggleMenu}>
+                <div className="hamburger-line" />
+                <div className="hamburger-line" />
+                <div className="hamburger-line" />
+            </div>
+            <div className="mobile-menu">
+                <a href="/#home">Home</a>
+                <a href="#about">About</a>
+                <a href="/schedule">Schedule</a>
+                <a href="/gallery">Gallery</a>
+                <a href="/#faq">FAQ</a>
+                <a href="/#sponsors">Sponsors</a>
+            </div>
             {/* <div className="register">
                 <Button text="Register" link="https://google.com" />
             </div> */}
