33d8ee62a4408af665409f76633f7ee5368609f9
Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-13 13:16:09 -0400

    Start adding mobile menu

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -40,18 +40,38 @@ export default function NavBar() {
                 <a href="/#sponsors">Sponsors</a>
             </ul>
 
+            {/* This is the mobile menu stuff, it isn't that much so not splitting into a component */}
             <div className="hamburger-menu" onClick={toggleMenu}>
                 <div className="hamburger-line" />
                 <div className="hamburger-line" />
                 <div className="hamburger-line" />
             </div>
-            <div className="mobile-menu">
+            <div
+                className={`mobile-menu ${
+                    menuOpen ? "mobile-menu-open" : "mobile-menu-closed"
+                }`}
+            >
+                <div className="mobile-menu-top">
+                    <img
+                        className="navbar-icon"
+                        src={Logo}
+                        alt="Eureka! Icon"
+                        href="/#home"
+                    />
+                    <div className="close" onClick={toggleMenu}>
+                        <div className="close-line-1" />
+                        <div className="close-line-2" />
+                    </div>
+                </div>
                 <a href="/#home">Home</a>
                 <a href="#about">About</a>
                 <a href="/schedule">Schedule</a>
                 <a href="/gallery">Gallery</a>
                 <a href="/#faq">FAQ</a>
                 <a href="/#sponsors">Sponsors</a>
+                <div className="mobile-menu-bottom">
+                    {/* <Button text="Register" link="https://google.com" /> */}
+                </div>
             </div>
             {/* <div className="register">
                 <Button text="Register" link="https://google.com" />
