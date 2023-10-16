293b2e34caf3f776030f1a34449baf759ae6897e
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-10-16 17:41:18 -0400

    Navbar and footer changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsLinkedin, BsTiktok, BsDiscord } from "react-icons/bs";
 
 export default function Footer() {
     return (
@@ -8,11 +8,36 @@ export default function Footer() {
             <div className="footer-container">
                 <div className="footer-socials">
                     <a
-                        href="https://www.instagram.com/eureka_hackathon/"
+                        className="instagram"
+                        href="https://www.instagram.com/eureka_hacks/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         <BsInstagram />
+                    </a>
+                    <a
+                        className="linkedin"
+                        href="https://www.linkedin.com/company/eureka-hackthon/about/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <BsLinkedin />
+                    </a>
+                    <a
+                        className="tiktok"
+                        href="https://www.tiktok.com/@eurekahacks?lang=en"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <BsTiktok />
+                    </a>
+                    <a
+                        className="discord"
+                        href="https://discord.gg/nff6VueNgk"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <BsDiscord />
                     </a>
                 </div>
                 <div className="footer-copyright">
