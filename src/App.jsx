68bb819f308ab0a161720dbd2008f88976e1d038
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-10-16 21:20:20 -0400

    Update mobile design a bit
    Remember to fix image later

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path ="/gallery" element={<Gallery />} />
+                <Route path="/gallery" element={<Gallery />} />
             </Routes>
             <Footer />
         </div>
