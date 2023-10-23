cf8b00a8ff2eb27c34836c6ffa0b01c5bc5a0020
Leg3ndary <ben.zhou.2006@gmail.com>
2023-10-23 13:53:47 -0400

    Update license and add new About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,14 +8,25 @@ export default function About() {
             <div className="about-left">
                 <h2>Oakvilles Biggest Hackathon</h2>
                 <p>
-                    Welcome to Eureka! Come to Abbey Park High School for a 12
-                    hour in person hackathon event! Here, you'll get to create a
-                    solution or project related to our theme, and get the chance
-                    to win prizes too! With workshops, coding challenges, and
-                    networking opportunities, participants will have the chance
-                    to unleash their full potential and bring their ideas to
-                    life. So gear up, get ready to code, and let's build the
-                    future together!
+                    EurekaHACKS is an annual 12-hour hackathon hosted at Abbey
+                    Park High School in Oakville, Canada. It is the largest high
+                    school hackathon in this region and a great chance to
+                    discover your potential with technology. Our exciting event
+                    features workshops, fun activities, and over $1000 worth of
+                    prizes! We're committed to inspiring students of all skill
+                    levels to put their creativity and knowledge into practice
+                    by giving them the tools they need to learn and create — no
+                    prior experience required.
+                </p>
+                <p>
+                    We recognize that young people have the potential to shape
+                    our future through innovative tech solutions, so our mission
+                    is to provide a platform where ambitious high schoolers can
+                    explore their ideas and take tangible steps towards building
+                    technological solutions for the world's biggest problems.
+                    Our vision is for every student who participates in
+                    EurekaHACKS to gain valuable skills while having some fun
+                    along the way!
                 </p>
             </div>
             <div className="about-right">
