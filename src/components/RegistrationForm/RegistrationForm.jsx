2afc854f0a95ec1418fe6d2ca5d5fda36d0d70b8
OrioleNFinch <112213597+OrioleNFinch@users.noreply.github.com>
2023-11-22 15:44:37 -0500

    form stuff

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,8 +1,19 @@
-
+import React from "react";
+import styles from "./RegistrationForm.module.css";
+import "../../pages/_app.jsx";
 
 export default function RegistrationForm() {
     return (
-        <>
-        </>
-    )
+        <section className={styles.form}>
+            <div className={styles.banner}>
+                <h2>SIGN UP</h2>
+            </div>
+
+            <div >
+                <input className={styles.field} type="text" placeholder="Name" name="name" required></input>
+                <input className={styles.field} type="text" placeholder="Email" name="email"  required></input>
+                <select className={styles.field} name="grade" placeholder="Grade"></select>
+            </div>
+        </section>
+    );
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -6,6 +6,8 @@ export default function Register() {
     return (
         <>
             <HomeIcon />
+            <hr />
+            <RegistrationForm />
         </>
     )
 }
