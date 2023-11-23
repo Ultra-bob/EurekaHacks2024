b5570aad91168c1e701aafa2344a39f190c2b2e4
OrioleNFinch <112213597+OrioleNFinch@users.noreply.github.com>
2023-11-23 16:44:10 -0500

    some more inputs

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./RegistrationForm.module.css";
 import "../../pages/_app.jsx";
 
+
+
 export default function RegistrationForm() {
     return (
         <section className={styles.form}>
@@ -12,7 +14,17 @@ export default function RegistrationForm() {
             <div >
                 <input className={styles.field} type="text" placeholder="Name" name="name" required></input>
                 <input className={styles.field} type="text" placeholder="Email" name="email"  required></input>
-                <select className={styles.field} name="grade" placeholder="Grade"></select>
+                <select className={styles.dropdown} name="grade" required>
+                    <option value="" disabled selected hidden>Grade</option>
+                        <option className={styles.options} value="0">9</option>
+                        <option className={styles.options} value="1">10</option>
+                        <option className={styles.options} value="2">11</option>
+                        <option className={styles.options} value="3">12</option>
+                </select>
+                <div className={styles.check}>
+                    <input type="checkbox" id="idk" required></input>
+                    <label className={styles.label} for="idk">lorem ipsum idk</label>
+                </div>
             </div>
         </section>
     );
