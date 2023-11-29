a0f92ff024da404d14871a7f39bd2543fb81c5a6
Yohance Pawania <109170289+ypawania@users.noreply.github.com>
2023-11-29 13:06:12 -0500

    Finish isRegistered route

--- src/pages/api/isRegistered.js
+++ src/pages/api/isRegistered.js
@@ -6,15 +6,35 @@ GET /api/isRegistered returns true or false
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
 
+async function checkRegistration(email) {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SHEETS_ID}/values/Sheet1?key=${process.env.SHEETS_KEY}`;
+    let data;
+    console.log(process.env.SHEETS_KEY)
+    const columnID = 0;
+    try {
+        const res = await fetch(url);
+        data = await res.json();
+        console.log(data);
+    }
+    catch (err) {
+        console.log(err);
+    }
+    for (let i = columnID; i < data.values.length; i++) {
+        if (data.values[i][0] === email) {
+            return true;
+        }
+    }
+    return false;
+}
+
 export default async function handler(req, res) {
     const session = await getServerSession(req, res, authOptions);
-
-    // Yohance add in whatever you need here to get the value from sheets
-    const checkRegistration = true;
+    
 
     if (session) {
+        const registered = await checkRegistration(session.user.email);
         res.status(200).send({
-            isRegistered: checkRegistration,
+            isRegistered: registered,
             email: session.user.email,
         });
     } else {
