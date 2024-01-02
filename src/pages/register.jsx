dc9d447e9c8ff93fd36d400d95a4d8d3a2e773ba
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-01 22:01:29 -0500

    Tally :thumbs_down:

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,8 +3,16 @@ import styles from "@/styles/register.module.css";
 import NavBar from "@/components/NavBar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import Script from "next/script";
+import { useState, useEffect } from "react";
 
 export default function Register() {
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        const scriptTag = document.createElement("script");
+        scriptTag.src = "https://tally.so/widgets/embed.js";
+        scriptTag.addEventListener("load", () => setLoaded(true));
+        document.body.appendChild(scriptTag);
+    }, []);
     return (
         <div className={styles.tally}>
             <Head>
@@ -13,18 +21,14 @@ export default function Register() {
                     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
                 />
                 <title>EurekaHACKS 2024 Registration Form</title>
+                <style>
+                    {`
+                        iframe {
+                            border: none;
+                        }
+                    `}
+                </style>
             </Head>
-            <NavBar />
-            <iframe
-                data-tally-src="https://tally.so/embed/nGlK5o?transparentBackground=1&dynamicHeight=1"
-                width="100%"
-                height="2464"
-                frameborder="0"
-                marginheight="0"
-                marginwidth="0"
-                title="EurekaHACKS 2024 Registration Form"
-            />
-            <Footer />
             <Script
                 id="tally-js"
                 src="https://tally.so/widgets/embed.js"
@@ -32,6 +36,15 @@ export default function Register() {
                     Tally.loadEmbeds();
                 }}
             />
+            <NavBar />
+            <iframe
+                data-tally-src="https://tally.so/embed/nGlK5o?transparentBackground=1&dynamicHeight=1"
+                width="100%"
+                height="2464"
+                loading="eager"
+                title="EurekaHACKS 2024 Registration Form"
+            />
+            <Footer />
         </div>
     );
 }
