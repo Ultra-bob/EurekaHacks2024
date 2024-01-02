3c3215d04e38bbada105de4a2895bfb21931aac3
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-01 20:31:58 -0500

    Add cool new register thing

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: "/register",
-                destination: "https://tally.so/r/nGlK5o",
-                permanent: true,
-            },
-        ];
-    },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: "/register",
+    //             destination: "https://tally.so/r/nGlK5o",
+    //             permanent: true,
+    //         },
+    //     ];
+    // },
 };
 
 module.exports = nextConfig;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,3 +1,29 @@
+import Head from "next/head";
+import styles from "@/styles/register.module.css";
+
 export default function Register() {
-    // This just redirects to the register page
+    return (
+        <div className={styles.tally}>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+                />
+                <title>EurekaHACKS 2024 Registration Form</title>
+                <script async src="https://tally.so/widgets/embed.js"></script>
+                <style>
+                    {`
+                        html { margin: 0; height: 100%; overflow: hidden; }
+                        iframe { position: absolute; top: 0; right: 0; bottom: 0; left: 0; border: 0; }
+                    `}
+                </style>
+            </Head>
+            <iframe
+                data-tally-src="https://tally.so/r/nGlK5o?transparentBackground=1"
+                width="100%"
+                height="100%"
+                title="EurekaHACKS 2024 Registration Form"
+            ></iframe>
+        </div>
+    );
 }
